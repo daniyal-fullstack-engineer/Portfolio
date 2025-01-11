864c08595ccf779d50429385d22b59e63c834a17
hamidRazach <150281909+HamidRazach@users.noreply.github.com>
2025-01-12 00:45:29 +0500

    cv add

--- src/Component/HireMe.jsx
+++ src/Component/HireMe.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 export default function HireMe() {
+  const handleViewCV = () => {
+    window.open('/src/assets/M.HAMID-RAZA.pdf', '_blank');
+  };
+
   return (
-    
-    <section class="call-to-action section-padding">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-8 col-lg-9">
-                    <div class="call-action-content text-center">
-                        <h2 class="action-title">Have any project in mind?</h2>
-                        <p>I'd love to hear about it! Whether it's a small idea or a grand vision, I'm ready to bring it
-                            to life. Reach out today and let's make your project a reality!</p>
-                        <ul>
-                            <li>
-                                <a href="assets/M.HAMID RAZA.pdf" class="btn btn-1" target="_blank">View CV</a>
-                            </li>
-                            <li><a href="#contact" class="btn btn-2" data-scroll-nav="6">HIRE ME</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <section className="call-to-action section-padding">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xl-8 col-lg-9">
+            <div className="call-action-content text-center">
+              <h2 className="action-title">Have any project in mind?</h2>
+              <p>
+                I'd love to hear about it! Whether it's a small idea or a grand vision, I'm ready to bring it
+                to life. Reach out today and let's make your project a reality!
+              </p>
+              <ul>
+                <li>
+                  <button onClick={handleViewCV} className="btn btn-1">
+                    View CV
+                  </button>
+                </li>
+                <li>
+                  <a href="#contact" className="btn btn-2" data-scroll-nav="6">
+                    HIRE ME
+                  </a>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
