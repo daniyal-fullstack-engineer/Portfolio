e8751fc88aae252a19f6f7516257b6c861e6bce5
M Daniyal SoftWare Engineer [React ,WordPress, PHP] <61905200+daniyal-sudo@users.noreply.github.com>
2025-01-12 20:34:33 +1100

    Add aniammtion

--- src/MainPages/Home.jsx
+++ src/MainPages/Home.jsx
@@ -1,61 +1,76 @@
-import React from 'react';
-import Header from '../Component/Header';
-import Banner from '../Component/Banner';
-import WhatAppIcon from '../Component/WhatAppIcon';
-import BackToTop from '../Component/BackToTop';
-import Footer from '../Component/Footer';
-import About from '../Component/About';
-import Services from '../Component/Services';
-import HireMe from '../Component/HireMe';
-import Portfolio from '../Component/Portfolio';
-import Testimonials from '../Component/Testimonials';
-import Pricing from '../Component/Pricing';
-import Faq from '../Component/Faq';
-import Contact from '../Component/Contact';
-
-
+import React from "react";
+import Header from "../Component/Header";
+import Banner from "../Component/Banner";
+import WhatAppIcon from "../Component/WhatAppIcon";
+import BackToTop from "../Component/BackToTop";
+import Footer from "../Component/Footer";
+import About from "../Component/About";
+import Services from "../Component/Services";
+import HireMe from "../Component/HireMe";
+import Portfolio from "../Component/Portfolio";
+import Testimonials from "../Component/Testimonials";
+import Pricing from "../Component/Pricing";
+import Faq from "../Component/Faq";
+import Contact from "../Component/Contact";
+import { FadeInSection } from "../Component/FadeIn";
 
 const Home = () => {
   return (
-    <div className='main-wrapper'>
-      <div className='navbar-section'>
-      <Header />
+    <div className="main-wrapper">
+      <div className="navbar-section">
+        <Header />
       </div>
-      <div className='banner-section'>
+      <div className="banner-section">
         <Banner />
       </div>
-      <div className='about-section'>
-        <About />
+      <div className="about-section">
+        <FadeInSection>
+          <About />
+        </FadeInSection>
       </div>
-      <div className='Service-section'>
-       <Services />
+      <div className="Service-section">
+        <FadeInSection>
+          <Services />
+        </FadeInSection>
       </div>
-      <div className='portfolio-section'>
-       <Portfolio />
+      <div className="portfolio-section">
+        <FadeInSection>
+          <Portfolio />
+        </FadeInSection>
       </div>
-      <div className='hire-me-section'>
-     <HireMe />
+      <div className="hire-me-section">
+        <FadeInSection>
+          <HireMe />
+        </FadeInSection>
       </div>
-      <div className='testimonial-section'>
-      <Testimonials />
+      <div className="testimonial-section">
+      <FadeInSection>
+        <Testimonials />
+        </FadeInSection>
       </div>
-      <div className='pricing-section'>
-       <Pricing />
+      <div className="pricing-section">
+      <FadeInSection>
+        <Pricing />
+        </FadeInSection>
       </div>
-      <div className='fag-section'>
-       <Faq />
+      <div className="fag-section">
+      <FadeInSection>
+        <Faq />
+        </FadeInSection>
       </div>
-      <div className='contact-section'>
-       <Contact />
+      <div className="contact-section">
+      <FadeInSection>
+        <Contact />
+        </FadeInSection>
       </div>
-      <div className='footer-section'>
+      <div className="footer-section">
         <Footer />
       </div>
-      <div className='backtotop-section'>
-       <BackToTop />
+      <div className="backtotop-section">
+        <BackToTop />
       </div>
-      <div className='WhattAppIcon-section'>
-      <WhatAppIcon />
+      <div className="WhattAppIcon-section">
+        <WhatAppIcon />
       </div>
     </div>
   );
