c29c7b775a86d3db44acb2f824b30ac606323435
M Daniyal SoftWare Engineer [React ,WordPress, PHP] <61905200+daniyal-sudo@users.noreply.github.com>
2025-01-14 04:15:21 +1100

    add dark mode

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  }
   return (
     <nav className="navbar navbar-expand-lg fixed-top">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <a  className="navbar-brand"
+        onClick={()=>{toggleTheme()}}>
           Hamid Portfolio
         </a>
         <button
